87049aee7c001f990aee3dca376115e5cb1474c4
khurshid28 <khurshidi2827@gmail.com>
2026-02-14 17:54:11 +0500

    fix: add TypeScript types to device service parameters

--- src/service/device.service.ts
+++ src/service/device.service.ts
@@ -1,24 +1,24 @@
 import axios from './axios.service';
 
 export const deviceService = {
-  getAllDevices: (params) => 
+  getAllDevices: (params: any) => 
     axios.get('/device/all', { params }),
 
-  getUserDevices: (userId, role) => 
+  getUserDevices: (userId: number, role: string) => 
     axios.get(`/device/user/${userId}/${role}`),
 
-  activateDevice: (id) => 
+  activateDevice: (id: number) => 
     axios.patch(`/device/${id}/activate`),
 
-  deactivateDevice: (id) => 
+  deactivateDevice: (id: number) => 
     axios.patch(`/device/${id}/deactivate`),
 
-  blockDevice: (id) => 
+  blockDevice: (id: number) => 
     axios.patch(`/device/${id}/block`),
 
-  unblockDevice: (id) => 
+  unblockDevice: (id: number) => 
     axios.patch(`/device/${id}/unblock`),
 
-  deleteDevice: (id) => 
+  deleteDevice: (id: number) => 
     axios.delete(`/device/${id}`),
 };
